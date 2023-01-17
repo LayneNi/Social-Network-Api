a049aca0acde96e417144821b49803e96498dd26
layne nigro <laynenigro@gmail.com>
2023-01-16 20:28:34 -0500

    added more boilerplate code

--- models/thought.js
+++ models/thought.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtsSchema = new Schema(
+const thoughtSchema = new Schema(
   {
     thoughtText: { type: String,  required: true, minLength: 1, maxLength: 280, },
     createdAt: { type: date, default: Date.now },
@@ -14,3 +14,7 @@ const thoughtsSchema = new Schema(
     id: false,
   }
 );
+
+const Thought = model('thought', thoughtSchema)
+
+module.export = Thought;
